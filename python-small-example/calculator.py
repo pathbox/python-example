ab12de546b43aa8a76a58a8dac61004f87aea1c7
pathbox <cary.luo@ucloud.cn>
2020-01-24 11:45:46 +0800

    simple calculator with operator

---
+++ python-small-example/calculator.py
@@ -0,0 +1,15 @@
+from operator import *
+
+
+def calculator(a, b, k):
+    return {
+        "+": add,
+        "-": sub,
+        "*": mul,
+        "/": truediv,
+        "**": pow
+    }[k](a, b)
+
+
+print(calculator(1, 2, '+'))
+print(calculator(3, 4, '**'))
